9229c59e6c500bba25d854627230183134b76739
RyanLittleKing <42249924+RLKGames@users.noreply.github.com>
2025-07-11 21:55:17 +0100

    Create NumberFactorsGenerator.py

---
+++ Python files/NumberFactorsGenerator.py
@@ -0,0 +1,23 @@
+import datetime
+import time
+
+dateTimeNow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+filePath = "PrimeNumberGeneratorOutput-" + str(dateTimeNow)
+filePath2 = filePath + ".txt"
+filePath2 = filePath2.replace(":","-")
+filePath2 = filePath2.replace(" ","-")
+
+with open(filePath2,"a") as f:
+    lowestNum = int(input("What number would you like to check? "))
+    startTime = time.perf_counter()
+    for numChecking in range(1,lowestNum+1):
+        if lowestNum % numChecking == 0:
+            output = str(numChecking) + " is a factor of " + str(lowestNum)
+        else:
+            output = str(numChecking) + " is not a factor of " + str(lowestNum)
+        print(output)
+        f.write(output + "\n")
+    endTime = time.perf_counter()
+    timeElapsed = round(endTime - startTime,2)
+    print("Time taken: " + str(timeElapsed) + "s")
+    f.write("Time taken: " + str(timeElapsed) + "s")
