984fee93c0129e1501c4816f0cfd7758dc6556c8
RyanLittleKing <42249924+RLKGames@users.noreply.github.com>
2025-08-16 12:28:29 +0100

    Update NumberGuessingGame.py

--- Python files/NumberGuessingGame.py
+++ Python files/NumberGuessingGame.py
@@ -1,23 +1,61 @@
 import random
+import sys
+import time
 
-attempts = 0
+def LBL(printInput):
+    for x in (str(printInput)):
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.005)
+    print()
+
+def LBLInput(printInput):
+    for x in (str(printInput)):
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.005)
+    output = input()
+    return output
+
+def LBLIntInput(printInput):
+    for x in (str(printInput)):
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.005)
+    output = int(input())
+    return output
+
+ERRORCODE1 = "Error code 1: If this message appears then please report this as a bug at https://github.com/RLKGames/RandomPythonFiles/issues"
+
 correct = False
-highestNum = int(input("What should be the highest number? "))
-if highestNum <= 1:
-    print("The number has to be more than 1")
-    quit()
-num = random.randint(1,highestNum)
+running = True
+attempts = 0
 
-while correct == False:
-    guess = int(input("\nEnter a guess between 1 and " + str(highestNum) + " "))
-    attempts += 1
-    if guess > num:
-        print("Too high")
-    elif guess < num:
-        print("Too low")
-    elif guess == num:
-        print("Correct!")
-        print("You took " + str(attempts) + " attempts to guess a number between 1 and "+ str(highestNum))
-        correct = True
+while running == True:
+    highestNum = int(input("What should the highest number be? "))
+    if highestNum <= 1:
+        print("The number has to be more than 1")
+        quit()
+    num = random.randint(1,highestNum)
+
+    while correct == False:
+        guess = int(input("\nEnter a guess between 1 and " + str(highestNum) + " "))
+        attempts += 1
+        if guess > num:
+            print("Too high")
+        elif guess < num:
+            print("Too low")
+        elif guess == num:
+            print("Correct!")
+            print("You took " + str(attempts) + " attempts to guess a number between 1 and "+ str(highestNum))
+            correct = True
+        else:
+            print(ERRORCODE1)
+
+    runAgainQ = input("\n\nWould you like to play again? ")
+    if runAgainQ == "y" or runAgainQ == "yes" or runAgainQ == "yep" or runAgainQ == "yeah":
+        running = True
+        print("\n\n\n\n")
     else:
-        print("If this message appears then please report this as a bug at https://github.com/RLKGames/RandomPythonFiles/issues")
+        running = False
+        print("\nOk, quitting\n")
