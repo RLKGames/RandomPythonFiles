52b1270470d5b4715b960ed2976ef9f48a4b68b6
RyanLittleKing <42249924+RLKGames@users.noreply.github.com>
2026-02-16 22:49:24 +0000

    Create CoinFlip.py

---
+++ Python files/CoinFlip.py
@@ -0,0 +1,90 @@
+import datetime
+import random
+import sys
+import time
+
+# error printer
+def errorPrint(errorID):
+    f = open("Python files/ErrorCodes.txt")
+    errorPrint = f.readlines(errorID).replace("[", "").replace("]", "").replace("\\n", "").replace("'", "")
+    f.readlines(errorID)
+
+# one by one character printer
+def LBL(printInput):
+    for x in (str(printInput)):
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.005)
+    print()
+
+# one by one character printer with input
+def LBLInput(printInput):
+    for x in (str(printInput)):
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.005)
+    output = input()
+    return output
+
+# one by one character printer with integer input
+def LBLIntInput(printInput):
+    for x in (str(printInput)):
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.005)
+    output = int(input())
+    return output
+
+# quit program
+def quitProgram():
+        LBL("\nQuitting\n")
+        quit()
+
+# print output to file
+def printToFile(dateTimeNow, output):
+    print(output)
+    filePath = "CoinFlipOutput-" + str(dateTimeNow) + ".txt"
+    with open(filePath, "a") as f:
+        f.write(output + "\n")
+
+# coin
+def coinFlip():
+    dateTimeNow = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    diceCount = LBLIntInput("How many coins should be flipped? ")
+    count = 0
+    heads = 0
+    tails = 0
+    startTime = time.perf_counter()
+    for i in range(0, diceCount):
+        coinFlip = random.randint(1, 2)
+        count += 1
+        if coinFlip == 1:
+            output = "Coin number " + str(count) + " landed on heads"
+            printToFile(dateTimeNow, output)
+            heads += 1
+        elif coinFlip == 2:
+            output = "Coin number " + str(count) + " landed on tails"
+            printToFile(dateTimeNow, output)
+            tails += 1
+
+    output = str(heads) + " coin(s) landed on heads"
+    printToFile(dateTimeNow, output)
+    output = str(tails) + " coin(s) landed on tails"
+    printToFile(dateTimeNow, output)
+
+    endTime = time.perf_counter()
+    timeTaken = round(endTime - startTime, 2)
+    output = "Took: " + str(timeTaken) + "s"
+    printToFile(dateTimeNow, output)
+
+    runAgain()
+
+# run again prompt
+def runAgain():
+    runAgainQ = LBLInput("\n\nWould you like to flip coins again? ")
+    if runAgainQ == "y" or runAgainQ == "yes" or runAgainQ == "yep" or runAgainQ == "yeah":
+        coinFlip()
+    else:
+        quitProgram()
+
+coinFlip()
